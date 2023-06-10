51a62177750d4bd6bceff92d9b3bd3f6495fc504
Azam Gulov <gulovazam@gmail.com>
2023-06-10 20:25:55 +0500

    Change buildTree to function declaration
    Fix index bug

--- tree.js
+++ tree.js
@@ -1,6 +1,21 @@
+import Node from "./node.js";
+
 const Tree = (array) => {
   const root = buildTree(array);
-  const buildTree = (arr) => {};
+
+  function buildTree(arr) {
+    if (arr.length < 1) return null;
+
+    const midPoint = Math.floor(arr.length / 2);
+    const leftHalf = arr.slice(0, midPoint);
+    const rightHalf = arr.slice(midPoint + 1);
+
+    const newNode = Node(arr[midPoint]);
+    newNode.leftNode = buildTree(leftHalf);
+    newNode.rightNode = buildTree(rightHalf);
+
+    return newNode;
+  }
 
   return {
     root,
