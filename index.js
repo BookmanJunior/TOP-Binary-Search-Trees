11b7430da851259217d17bc87dd3bcc25419215a
Azam Gulov <gulovazam@gmail.com>
2023-06-11 18:36:02 +0500

    Add tests

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import Tree from "./tree.js";
 import prettyPrint from "./prettyPrint.js";
 
-const orderedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const orderedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const unorderedArray = [1, 7, 4, 23, 8, 9, 3, 5, 7, 67, 6345, 324];
+const orderedArrayDupes = [1, 2, 2, 3, 4, 4, 4, 5, 5];
 
-const newTree = Tree(orderedArray);
+const newTree = Tree(orderedArrayDupes);
 prettyPrint(newTree.root);
