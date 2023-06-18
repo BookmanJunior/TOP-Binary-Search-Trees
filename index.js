2de9b8f2a067fe31278b99255b645ae50a0b7afd
Azam Gulov <gulovazam@gmail.com>
2023-06-18 18:43:14 +0500

    Added tests

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 import Tree from "./tree.js";
 import prettyPrint from "./prettyPrint.js";
 
-// const orderedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// const unorderedArray = [1, 7, 4, 23, 8, 9, 3, 5, 7, 67, 6345, 324];
-const orderedArrayDupes = [1, 2, 2, 3, 4, 4, 4, 5, 5];
+const randomNumbers = (num, max = 100) => {
+  const arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push(Math.floor(Math.random() * max));
+  }
+  return arr;
+};
 
-const newTree = Tree(orderedArrayDupes);
-prettyPrint(newTree.root);
+const printTreeInAllLevels = (tree) => {
+  console.log(`Post Order: ${tree.postOrder()}`);
+  console.log(`In Order: ${tree.inOrder()}`);
+  console.log(`Pre Order: ${tree.preOrder()}`);
+  console.log(`Post Order: ${tree.postOrder()}\n`);
+};
+
+const checkTreesBalance = (tree) => {
+  console.log(`Is Tree Balanced: ${tree.isBalanced()}`);
+};
+
+const binaryTree = () => {
+  const newTree = Tree(randomNumbers(10));
+
+  // confirm that the tree is balanced
+  checkTreesBalance(newTree);
+
+  // print out elements in level, in, pre and post order
+  printTreeInAllLevels(newTree);
+
+  // unbalance tree by adding several numbers > 100
+  newTree.insert(Math.floor(Math.random() * 1000));
+  newTree.insert(Math.floor(Math.random() * 1000));
+  newTree.insert(Math.floor(Math.random() * 1000));
+
+  // confirm that the tree is unbalanced
+  checkTreesBalance(newTree);
+
+  // balance the tree
+  newTree.rebalance();
+
+  // confirm that tree is balanced
+  checkTreesBalance(newTree);
+
+  // print out elements in level, in, pre and post order
+  printTreeInAllLevels(newTree);
+
+  // prettyprint
+  prettyPrint(newTree.root);
+};
+
+binaryTree();
